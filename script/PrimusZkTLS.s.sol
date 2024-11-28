372943ce1fac20dcd0a1b4251d72829e6cc57fa3
WuEcho <emailforecho@163.com>
2024-11-28 12:29:41 +0800

    Update PrimusZkTLS.s.sol

--- script/PrimusZkTLS.s.sol
+++ script/PrimusZkTLS.s.sol
@@ -40,3 +40,26 @@ contract DeployPrimusZkTLS is Script {
         vm.stopBroadcast();
     }
 }
+
+contract UpgradePrimusZkTLS is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        // 1. Deploy the new logic contract
+        PrimusZkTLS newLogic = new PrimusZkTLS();
+
+        // 2. Retrieve the ProxyAdmin address and Proxy contract address
+        address proxyAdminAddr = address(0x11111); // Replace with the actual ProxyAdmin address
+        address proxyAddr = address(0x2222);           // Replace with the actual Proxy address
+
+        // 3. Call the upgrade method of ProxyAdmin
+        ProxyAdmin proxyAdmin = ProxyAdmin(proxyAdminAddr);
+        ITransparentUpgradeableProxy proxy = ITransparentUpgradeableProxy(proxyAddr);
+        proxyAdmin.upgradeAndCall(proxy, address(newLogic),"");
+
+        console.log("Upgraded Proxy to New Logic Address: ", address(newLogic));
+
+        vm.stopBroadcast();
+    }
+}
