bb919cdd13aa106373b1db32e51bf962a3868b6a
fksyuan <fksyuan@163.com>
2024-12-10 16:03:49 +0800

    update call script

--- script/CallZkTLS.s.sol
+++ script/CallZkTLS.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import { Script } from "forge-std/Script.sol";
-import "../src/PrimusZkTLS.sol";
+import "../src/PrimusZKTLS.sol";
 
 contract CallZkTLS is Script {
     function run() public {
@@ -10,7 +10,7 @@ contract CallZkTLS is Script {
         //address senderAddress = vm.addr(senderPrivateKey);
         vm.startBroadcast(senderPrivateKey);
 
-        PrimusZkTLS primusZkTLS = PrimusZkTLS(address(0x11701Cf8Df597668C8dCCdD67Fd0836fB8564f80));
+        PrimusZKTLS primusZkTLS = PrimusZKTLS(address(0x11701Cf8Df597668C8dCCdD67Fd0836fB8564f80));
         Attestor memory newUser = Attestor({
             attestorAddr: address(0xe02bD7a6c8aA401189AEBb5Bad755c2610940A73),
             url: "https://primuslabs.org"
