649067f972ad1ba3731a1b710920dfc9378e7cca
fksyuan <fksyuan@163.com>
2025-03-14 14:25:13 +0800

    add sophon deploy script and fix the deploy script

--- script/PrimusZKTLS-Sophon.s.sol
+++ script/PrimusZKTLS-Sophon.s.sol
@@ -5,16 +5,25 @@ import "forge-std/Script.sol";
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import "../src/PrimusZKTLS.sol";
+import {TestExt} from "lib/forge-zksync-std/src/TestExt.sol";
 
-contract DeployPrimusZKTLS is Script {
+contract DeployPrimusZKTLS is Script, TestExt {
     function run() external {
         // 1. Get private key
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address deployerAddress = vm.addr(deployerPrivateKey);
         vm.startBroadcast(deployerPrivateKey);
 
+        // Encode paymaster input
+        bytes memory paymaster_encoded_input = abi.encodeWithSelector(
+            bytes4(keccak256("general(bytes)")),
+            bytes("0x")
+        );
+        vmExt.zkUsePaymaster(vm.envAddress("PAYMASTER_ADDRESS"), paymaster_encoded_input);
+
         // 2. Deploy logic contract (implementation)
-        PrimusZKTLS logic = new PrimusZKTLS();
+        // PrimusZKTLS logic = new PrimusZKTLS();
+        // sophontestnet logic: 0x73bb5892283c0d5420AB9B8B66F662b90D937096
 
         // 3. Prepare initialization data
         bytes memory initializeData = abi.encodeWithSelector(
@@ -24,15 +33,16 @@ contract DeployPrimusZKTLS is Script {
 
         // 4. Deploy TransparentUpgradeableProxy
         TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
-            address(logic),
+            address(0x73bb5892283c0d5420AB9B8B66F662b90D937096),
             deployerAddress,
             initializeData
         );
+        // sophontestnet proxy: 0x7068da2522c3Ba1f24594ce20E7d7A8EF574E89f
 
         // 5. Log contract addresses
-        console.log("Logic Contract Address: ", address(logic));
+        // console.log("Logic Contract Address: ", address(logic));
         // console.log("Proxy Admin Address: ", address(proxyAdmin));
-        console.log("Proxy Contract Address: ", address(proxy));
+        // console.log("Proxy Contract Address: ", address(proxy));
 
         vm.stopBroadcast();
     }
