f0b77e047063a8a86c3c39702df84bf7c35c143a
fksyuan <fksyuan@163.com>
2026-01-05 14:26:07 +0800

    update compile file

--- script/CallZKTLS.s.sol
+++ script/CallZKTLS.s.sol
@@ -10,11 +10,18 @@ contract CallZkTLS is Script {
         //address senderAddress = vm.addr(senderPrivateKey);
         vm.startBroadcast(senderPrivateKey);
 
-        PrimusZKTLS primusZkTLS = PrimusZKTLS(address(0xB3d8DDDc793F75a930313785e5d1612747093f25));
+        // PrimusZKTLS primusZkTLS = PrimusZKTLS(address(0x1Ad7fD53206fDc3979C672C0466A1c48AF47B431));
+        // Attestor memory attestor = Attestor({
+        //     attestorAddr: address(0xe02bD7a6c8aA401189AEBb5Bad755c2610940A73),
+        //     url: "https://primuslabs.xyz/"
+        // });
+
+        PrimusZKTLS primusZkTLS = PrimusZKTLS(address(0x4E78940F0019EbAEDc6F4995D7B8ABf060F7a341));
         Attestor memory attestor = Attestor({
-            attestorAddr: address(0xe02bD7a6c8aA401189AEBb5Bad755c2610940A73),
+            attestorAddr: address(0xDB736B13E2f522dBE18B2015d0291E4b193D8eF6),
             url: "https://primuslabs.xyz/"
         });
+
         primusZkTLS.setAttestor(attestor);
 
         vm.stopBroadcast();
